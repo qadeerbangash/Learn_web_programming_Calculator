738ecfa61aee71469f868508a5527c2f32991e29
Qadeer Hussain <qadeeerhussain494@gmail.com>
2021-04-04 12:23:55 +0500

    Complete Work

---
+++ app.js
@@ -0,0 +1,17 @@
+function getnumber(num){
+    // console.log(num)
+    var result=document.getElementById("result")
+    result.value +=num;
+
+
+}
+function clearResult(){
+    var result=document.getElementById("result");
+    result.value="";
+}
+
+function getResult(){
+    var result=document.getElementById("result");
+    // console.log(result.value)
+    result.value=eval(result.value)
+}
